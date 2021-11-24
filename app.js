3c520b77a3756d1c389ec54fe57fafa7962a1d20
Vatsal <vatsaltailot6500@gmail.com>
2021-11-24 11:29:22 +0530

    some changes in app.js

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const helmet = require('helmet');
-const path = require('path');
 const cookieparser = require('cookie-parser');
 const bookRouter = require('./routes/BookRouter');
 const custRouter = require('./routes/CustRouter');
 const homeRouter = require('./routes/HomeRouter');
-const { sign,verify } = require('./libs/jwt');
+const { verify } = require('./libs/jwt');
 
 //app setting
 app.set("view engine", "pug");
@@ -16,8 +15,8 @@ app.set("views","./views");
 
 
 //middlewares
-app.use(express.urlencoded({extended:true})); // used insted of body parser
-app.use(express.json()); // for testing in postman(ANY) must required
+app.use(express.urlencoded({extended:false})); // used insted of body parser
+app.use(express.json()); 
 app.use(morgan("combined")); // think morgan as a helper that generates request logs.
 app.use(helmet());
 app.use(express.static('public'))
@@ -25,8 +24,8 @@ app.use(cookieparser());
 
 
 //routes
-app.use('/books', verify,bookRouter);
-app.use('/customers',custRouter);
+app.use('/books', verify, bookRouter);
+app.use('/customers', verify, custRouter);
 app.use('/home',homeRouter);
 app.get('/gettoken');
 
